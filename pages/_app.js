5c50b49275a41c7814e4921bf93b37f5050e410e
syoo970 <syu9710@gmail.com>
2022-08-21 17:13:29 +0900

    [style] prettier 적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,22 @@
+import { useState } from "react";
 import "../styles/globals.css";
 import { RecoilRoot } from "recoil";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
 import Navigation from "../components/Navigation";
 import { DefaultNav } from "../components/Navigation/NavLists";
 import KonerGlobalStyle from "../styles/konerGlobalStyle";
 
 function MyApp({ Component, pageProps }) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <KonerGlobalStyle />
-        <Navigation {...DefaultNav} />
-        <Component {...pageProps} />
-      </RecoilRoot>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <KonerGlobalStyle />
+          <Navigation {...DefaultNav} />
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </Hydrate>
     </QueryClientProvider>
   );
 }
